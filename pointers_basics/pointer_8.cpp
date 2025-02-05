4d31b291b87c4cb47fb14f12bc01a621ee4413ff
KunwarPrabhat <rajkunwarprabhat@gmail.com>
2025-02-06 02:36:22 +0530

    Added another two questions

---
+++ pointers_basics/pointer_8.cpp
@@ -0,0 +1,42 @@
+// Write a function that takes an array and its size as parameters and reverses the array using only pointers (without using extra arrays).
+#include<iostream>
+#define log(x) std::cout<<"\n";
+
+void reverseArray(int* arr, int size){
+    int* ptr1 = arr;
+    int* ptr2 = arr + size -1;
+    while(ptr1 < ptr2){
+        int temp = *ptr1;
+        *ptr1 = *ptr2;
+        *ptr2 = temp;
+
+        ptr1++;
+        ptr2--;
+    }
+}
+int main() {
+    int size;
+    std::cout<<"Enter the size of the array : "<<std::endl;
+    std::cin>>size;
+
+    int* arr = new int[size];
+    int* ptr = arr;
+
+    std::cout<<"Enter the elements : "<<std::endl;
+    for(int i = 0; i < size; i++){
+        std::cin >> *(ptr + i);
+    }
+
+    std::cout<<"Entered array : ";
+    for(int i = 0; i < size; i++){
+        std::cout<< *(ptr + i) <<" ";
+    }log(x);
+    
+    reverseArray(arr, size);
+    std::cout<<"The reversed array is : ";
+
+    for(int i=0; i<size; i++){
+        std::cout<< *(ptr + i)<<" ";
+    }
+
+}
---
+++ pointers_basics/pointer_9.cpp
@@ -0,0 +1,23 @@
+//Write a C++ program that takes a positive integer as input, reverses its digits using pointers, and prints the reversed number.
+#include<iostream>
+
+int main() {
+    int n;
+    int count = 0;
+    std::cout<<"Enter a number : ";
+    std::cin>> n;
+    int temp =n;
+
+    while (temp > 0) {
+        temp/= 10;  
+        count++;    
+    }
+    int* arr = new int[count];
+    int* ptr = arr;
+    std::cout<<"Reversed number : ";
+    for(int i = 0; i<count; i++){
+        *(ptr + i) = n % 10; 
+        std::cout<< *(ptr + i);
+        n/=10; 
+    }
+}
