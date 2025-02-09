54884cc03ccfac283d07da9edf746bcd2e22b5af
Raj Kunwar Prabhat <rajkunwarprabhat@gmail.com>
2025-02-09 20:55:46 +0530

    new question

--- arrays_pointers_1d_2d/pointer_15.cpp
+++ arrays_pointers_1d_2d/pointer_15.cpp
@@ -5,18 +5,29 @@ void printArray(int** a2d, int rows, int cols){
     std::cout<<"The array is : \n";
     for(int i = 0; i < rows; i++){
         for(int j = 0; j < cols; j++){
-            std::cout<<*(*(a2d + i) + j)<<" ";
+            std::cout<<*(*(a2d + i) + j)<<"  ";
         }
         std::cout<<std::endl;
     }
 }
 void rotate(int** a2d, int rows, int cols){
-    std::cout<<"The array is : \n";
+//step 1  Transpose the matrix. (swap a2d[i][j] with a2d[j][i])
     for(int i = 0; i < rows; i++){
-        for(int j = 0; j < cols; j++){
-            std::cout<<*(*(a2d + j) + i)<<" ";
+        for(int j = i+1; j < cols; j++){
+            // std::swap(*(*(a2d + i) + j), *(*(a2d + j) + i)) pre- built function for swapping.
+            //manually.
+            int temp = *(*(a2d + i) + j);
+            *(*(a2d + i) + j) = *(*(a2d + j) + i);
+            *(*(a2d + j) + i) = temp;
         }
-        std::cout<<std::endl;
+    }
+// Step 2: Reverse each row to complete 90-degree rotation.
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < cols / 2; j++){
+            int temp = *(*(a2d + i) + j);
+            *(*(a2d + i) + j) = *(*(a2d + i) + (cols -j - 1));
+            *(*(a2d + i) + (cols - j - 1)) = temp;
+        }
     }
 }
 int main (){ 
@@ -36,6 +47,8 @@ int main (){
     }
     printArray(a2d, rows, cols);
     rotate(a2d, rows, cols);
+    printArray(a2d, rows, cols);
+
     for(int i = 0; i < rows; i++){
         delete [] a2d[i];
     }
